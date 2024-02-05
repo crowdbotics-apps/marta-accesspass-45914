c2e5fd9d52c28ca3e772d253f921766620632c9c
Crowdbotics <team@crowdbotics.com>
2024-02-05 16:32:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "marta-accesspass-45914",
+  url: "https://marta-accesspass-45914.botics.co",
+  api: "https://marta-accesspass-45914.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
